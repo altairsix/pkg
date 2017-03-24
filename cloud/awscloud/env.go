8d17e0c26648e8fcd206c897fe27e6e774548284
savaki <matt.ho@gmail.com>
2017-03-24 07:59:05 -0700

    - added support for reading aws config from env

---
+++ cloud/awscloud/env.go
@@ -0,0 +1,31 @@
+package awscloud
+
+import (
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/pkg/errors"
+)
+
+func EnvRegion() string {
+	region := os.Getenv("AWS_DEFAULT_REGION")
+	if region == "" {
+		region = "us-west-2"
+	}
+
+	return region
+}
+
+// EnvSession returns an AWS session constructed from env variables
+func EnvSession() (*session.Session, error) {
+	region := EnvRegion()
+
+	cfg := &aws.Config{Region: aws.String(region)}
+	s, err := session.NewSession(cfg)
+	if err != nil {
+		return nil, errors.Wrap(err, "awscloud:env_session:err")
+	}
+
+	return s, nil
+}
