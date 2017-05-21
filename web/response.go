9f714cfac5dff0a6da89fec64a5ea6f869804aef
savaki <matt.ho@gmail.com>
2017-05-21 08:39:47 -0700

    - added BadRequest to the list of canned web responses

--- web/response.go
+++ web/response.go
@@ -3,11 +3,13 @@ package web
 import "net/http"
 
 type Response struct {
-	Status int `json:"status"`
+	Status  int    `json:"status"`
+	Message string `json:"message,omitempty"`
 }
 
 var (
-	Ok        = Response{Status: http.StatusOK}
-	NotFound  = Response{Status: http.StatusNotFound}
-	Forbidden = Response{Status: http.StatusForbidden}
+	Ok         = Response{Status: http.StatusOK}
+	NotFound   = Response{Status: http.StatusNotFound}
+	Forbidden  = Response{Status: http.StatusForbidden}
+	BadRequest = Response{Status: http.StatusBadRequest}
 )
