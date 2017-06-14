0a9a3981ec03cefcfe40ef8707e4000e932355ec
Matt Ho <matt.ho@gmail.com>
2017-06-13 18:46:56 -0700

    - added helper for IsDuplicateKey and IsDuplicateEntry

--- dbase/errs.go
+++ dbase/errs.go
@@ -3,10 +3,21 @@ package dbase
 import "github.com/go-sql-driver/mysql"
 
 const (
+	MySQLDuplicateKeyNum   = 1061
 	MySQLDuplicateEntryNum = 1062
 )
 
-func IsDuplicateErr(err error) bool {
+func IsDuplicateKey(err error) bool {
+	if mysqlErr, ok := err.(*mysql.MySQLError); ok {
+		if mysqlErr.Number == MySQLDuplicateKeyNum {
+			return true
+		}
+	}
+
+	return false
+}
+
+func IsDuplicateEntry(err error) bool {
 	if mysqlErr, ok := err.(*mysql.MySQLError); ok {
 		if mysqlErr.Number == MySQLDuplicateEntryNum {
 			return true
@@ -14,4 +25,8 @@ func IsDuplicateErr(err error) bool {
 	}
 
 	return false
+}
+
+func IsDuplicateErr(err error) bool {
+	return IsDuplicateEntry(err)
 }
