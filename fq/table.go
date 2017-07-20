d20186a0c29ee592a149746aa1a09fc1ab3b57b8
savaki <matt.ho@gmail.com>
2017-07-19 19:39:04 -0700

    added unified name for singletons

--- fq/table.go
+++ fq/table.go
@@ -7,3 +7,8 @@ import "strings"
 func DynamoDBTableName(env, service, table string) string {
 	return strings.Join([]string{env, service, table}, "-")
 }
+
+// SingletonTableName contains the name of the table that holds the eventsource singletons
+func SingletonTableName(env string) string {
+	return env + "-singleton"
+}
